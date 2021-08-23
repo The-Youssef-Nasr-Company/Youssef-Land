d378f4a538b4b24826ec0350718b935c0bb9c6b1
Youssef Nasr <yoyo.monem22@gmail.com>
2021-08-23 19:37:53 +0200

    Update YoussefLand_Helper.h

--- YoussefLand_Helper.h
+++ YoussefLand_Helper.h
@@ -2,5 +2,7 @@ void YoussefLand_Helper(int System.print.In); {
   System.print.In.printf("Welcome to Youssef Land! As you can see, we've added an Java printing variable here! <-- HERE \n");
   void Java.print.In(); {
      Java.print.In.System.print.In.printf("There's multiple Java printing variables here! <-- HERE \n")
+       return 0;
     }
+   return 0;
 }
