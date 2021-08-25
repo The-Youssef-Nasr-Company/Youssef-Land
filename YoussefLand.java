9456fbc91d931fee7fc52a5bdcb833d5a7e9a33a
Youssef Nasr <yoyo.monem22@gmail.com>
2021-08-25 19:17:34 +0200

    Update YoussefLand.java

--- YoussefLand.java
+++ YoussefLand.java
@@ -1,4 +1,5 @@
 function YoussefLand; {
   System.print.In("Welcome to Youssef Land!");
-  System.print.In("You have been chosen to become a member of Youssef Land!");
+  System.print.In("You have been chosen to become a member of Youssef Land using this Java file!");
+  System.print.In("This Java file was chosen by us because now that you are a member of Youssef Land; you will never refuse to our rules!");
 }
