adb93c0ba199d034aa0da947f9e59c4d620dd07a
Youssef Nasr <yoyo.monem22@gmail.com>
2021-08-25 19:21:11 +0200

    Create BadJoin.java

---
+++ BadJoin.java
@@ -0,0 +1,2 @@
+% You are no longer chosen to become a member. Your save data will be deleted as long as you refuse to our rules. {
+  System.print.In("You have been chosen to leave Youssef Land as long as you refuse to our rules! This is no good! What are we going to do?!");
