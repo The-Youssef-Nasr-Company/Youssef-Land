c98690210f390067f8e33a65d43d908f0a0f9d0a
Youssef Nasr <yoyo.monem22@gmail.com>
2021-08-28 12:31:25 +0200

    Update YoussefLand.js

--- YoussefLand.js
+++ YoussefLand.js
@@ -1,7 +1,7 @@
 function YoussefLand(); {
          System.print.In("You are using an Youssef Land actual JavaScript file (*.js).");
          System.print.In("This JavaScript file was made by Youssef.");
-         System.print.In("JavaScript was made by Oracle and was marked as an Java update. If you want this JavaScript language Youssef is using, try grabbing JavaScript's website.");
+         System.print.In("JavaScript was made by Oracle and was marked as an Java update. If you want this language Youssef is using, try grabbing JavaScript's website.");
          System.print.In("This is actual.");
          System.print.In("Because you are chosen to become a member on our side.");
 }
