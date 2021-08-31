6ef16ce7011484c344c4f6f5c961c391d08d5d0d
Youssef Nasr <yoyo.monem22@gmail.com>
2021-08-31 18:30:47 +0200

    Create YoussefLandJava.cpp

---
+++ YoussefLandJava.cpp
@@ -0,0 +1,9 @@
+#define YoussefLand
+
+#include "Youssef_Land_Helper.h"
+#include <stdio.h>
+void YoussefLand(int Java = true) {
+  printf("This is fun!\n");
+  if Java = true {
+    Java.printf("This is Java!/n")
+  }
