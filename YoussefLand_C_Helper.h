4dc1290740cda1bee80ea8b524a18513c25fdaee
Youssef Nasr <yoyo.monem22@gmail.com>
2021-09-06 18:33:21 +0200

    Update YoussefLand_C_Helper.h

--- YoussefLand_C_Helper.h
+++ YoussefLand_C_Helper.h
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-void YoussefLand_Helper(int System.print.In); {
+void YoussefLand_Helper(int System.print.In); 
+{
   System.print.In.printf("Welcome to Youssef Land! As you can see, we've added an Java printing variable here! <-- HERE");
-  void Java.print.In(); {
+  void Java.print.In(); 
+	{
      Java.print.In.System.print.In.printf("There's multiple Java printing variables here! <-- HERE")
        return 0;
-    }
+  }
    return 0;
 }
