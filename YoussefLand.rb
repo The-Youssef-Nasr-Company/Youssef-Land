7a662142f3ca4d62e132c30380016d6c56ad91cf
Youssef Nasr <yoyo.monem22@gmail.com>
2021-09-07 18:08:19 +0200

    Create YoussefLand.rb

---
+++ YoussefLand.rb
@@ -0,0 +1,15 @@
+# This is how the main Ruby script works.
+
+puts "Psst! Hey, welcome to Youssef Land!"
+
+print "We're here in Ruby now."
+
+puts "What's up?"
+
+print "Ruby is an brilliant programming lnguage."
+
+puts "How can I code with it?"
+
+print "Just look at this script."
+
+puts "OK."
