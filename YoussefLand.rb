068448e76c43b4de8a3f3f22e81a70a4c807da96
Youssef Nasr <yoyo.monem22@gmail.com>
2021-09-07 18:15:22 +0200

    Update YoussefLand.rb

--- YoussefLand.rb
+++ YoussefLand.rb
@@ -13,3 +13,9 @@ puts "How can I code with it?"
 print "Just look at this script."
 
 puts "OK."
+
+puts "*rattles Ruby program*"
+
+puts "OK, now I know how to code with Ruby."
+
+print "Great job!"
