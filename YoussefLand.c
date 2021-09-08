62e4e84dbcac2bb0b93c0b39a6bdd72465d57058
Youssef Nasr <yoyo.monem22@gmail.com>
2021-09-08 18:29:52 +0200

    Update YoussefLand.c

--- YoussefLand.c
+++ YoussefLand.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <YoussefLand.h>
-int main() {
+int main(); 
+{
    // printf() displays the string inside quotation
    printf("Welcome to Youssef Land!");
    return 0;
 }
+
