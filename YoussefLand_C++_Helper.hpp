134730937322d7d2410d044d47b81a151b6b6f9f
Youssef Nasr <yoyo.monem22@gmail.com>
2021-09-08 14:20:00 +0200

    Update and rename YoussefLand_C_Helper.hpp to YoussefLand_C++_Helper.hpp

---
+++ YoussefLand_C++_Helper.hpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void YoussefLand_Helper(int System.print.In); 
+{
+  System.print.In.printf("Welcome to Youssef Land! As you can see, we've added an Java printing variable here! <-- HERE\n");
+  void Java.print.In(); 
+	{
+     Java.print.In.System.print.In.printf("There's multiple Java printing variables here! <-- HERE\n")
+       return 0;
+  }
+   return 0;
+}
