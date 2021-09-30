cb18d2c362e17c4a12101ec167b6145029b55c08
Youssef Nasr <yoyo.monem22@gmail.com>
2021-09-30 18:56:40 +0200

    Update YoussefLand.java

--- YoussefLand.java
+++ YoussefLand.java
@@ -1,7 +1,7 @@
-% You have been chosen to become a member if you don't refuse to our rules.
+// You have been chosen to become a member if you don't refuse to our rules.
 
-function YoussefLand; {
-  System.print.In("Welcome to Youssef Land!");
-  System.print.In("You have been chosen to become a member of Youssef Land using this Java file!");
-  System.print.In("This Java file was chosen by us because now that you are a member of Youssef Land; you will never refuse to our rules!");
+public void YoussefLand(); {
+  System.out.println("Welcome to Youssef Land!");
+  System.out.println("You have been chosen to become a member of Youssef Land using this Java file!");
+  System.out.println("This Java file was chosen by us because now that you are a member of Youssef Land; you will never refuse to our rules!");
 }
