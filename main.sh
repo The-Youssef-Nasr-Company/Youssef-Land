77e95bce7b8a91d1c41d6c173a6574ebadccab7f
Youssef <yoyo.monem22@gmail.com>
2021-12-13 18:06:39 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 # Main script for Youssef Land.
 
-echo "Welcome to Youssef Land!";
-echo "This is the main batch script for Command Prompt/Terminal.";
+echo "Welcome to Youssef Land!"
+echo "This is the main UNIX script for Terminal."
