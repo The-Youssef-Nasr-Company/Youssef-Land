282cf7e75d4719192fd98f33396181b53a556482
Youssef <yoyo.monem22@gmail.com>
2022-05-13 20:42:45 +0200

    Update App.js

--- assets/YoussefLand/App.js
+++ assets/YoussefLand/App.js
@@ -1,5 +1,5 @@
 var err_code;
 err_code = ["404", "400", "500", "504"];
 var err_display;
-err_display = "A serious error"+err_code+"ocurred. There's no reason for this problem.";
-
+err_display = "A serious error "+err_code+" ocurred. There's no reason for this problem.";
+print(err_display);
