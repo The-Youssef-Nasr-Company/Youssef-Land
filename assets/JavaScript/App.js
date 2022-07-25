ae357198a6d2e6cf5880c5db043279f9bca43614
Youssef <yoyo.monem22@gmail.com>
2022-07-25 18:28:11 +0200

    Update App.js

--- assets/JavaScript/App.js
+++ assets/JavaScript/App.js
@@ -2,4 +2,4 @@ var err_code;
 err_code = ["404", "400", "500", "504"];
 var err_display;
 err_display = "A serious error "+err_code+" ocurred. There's no reason for this problem.";
-print(err_display);
+console.log(err_display);
